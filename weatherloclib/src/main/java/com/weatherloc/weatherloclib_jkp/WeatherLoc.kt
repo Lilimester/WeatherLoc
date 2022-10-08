e7ee5e2970ec6f391afc7c3a85228f95260568f6
Jaimin913 <jaimin913@gmail.com>
2022-10-09 04:32:17 +0800

    minor changes

--- weatherloclib/src/main/java/com/weatherloc/weatherloclib_jkp/WeatherLoc.kt
+++ weatherloclib/src/main/java/com/weatherloc/weatherloclib_jkp/WeatherLoc.kt
@@ -1,8 +1,12 @@
 package com.weatherloc.weatherloclib_jkp
 
+import com.weatherloc.weatherloclib_jkp.open.models.WeatherLatLngResponse
+
 open class WeatherLoc {
 
-    
+    fun obtainCurrentWeatherFromLatLng(lat:Double, lng:Double, weather: suspend(WeatherLatLngResponse)-> Unit){
+        
+    }
 
     companion object{
         val instance = WeatherLoc()
